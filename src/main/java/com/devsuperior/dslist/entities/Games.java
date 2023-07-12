ecd9eb751f19a98f00fdb8af271b65fa93e14370
isaccsz <isaccruz47@outlook.com>
2023-07-12 14:27:39 -0300

    Game Seed

--- src/main/java/com/devsuperior/dslist/entities/Games.java
+++ src/main/java/com/devsuperior/dslist/entities/Games.java
@@ -25,7 +25,11 @@ public class Games {
 	private String platforms;
 	private Double score;
 	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+	
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
 	
