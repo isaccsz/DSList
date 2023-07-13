567c21bf3dd44ee501586a09ad8ee20136a42d35
isaccsz <isaccruz47@outlook.com>
2023-07-13 16:30:03 -0300

    Get Lists

--- src/main/java/com/devsuperior/dslist/services/GamesService.java
+++ src/main/java/com/devsuperior/dslist/services/GamesService.java
@@ -1,7 +1,6 @@
 package com.devsuperior.dslist.services;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
