68d0d782921c9e17993cb8557010e8fac47e33cf
isaccsz <isaccruz47@outlook.com>
2023-07-13 17:33:15 -0300

    Add query methods, new interface, modified Controllers

--- src/main/java/com/devsuperior/dslist/services/GamesService.java
+++ src/main/java/com/devsuperior/dslist/services/GamesService.java
@@ -1,12 +1,15 @@
 package com.devsuperior.dslist.services;
 
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
 import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Games;
+import com.devsuperior.dslist.projections.GameMinProjection;
 import com.devsuperior.dslist.repositories.GameRepository;
 
 @Service
@@ -26,6 +29,12 @@ public class GamesService {
 	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll(){
 		List<Games> result = gameRepository.findAll();
+		return result.stream().map(x -> new GameMinDTO(x)).toList();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findByList(Long listId){
+		List<GameMinProjection> result = gameRepository.searchByList(listId);
 		return result.stream().map(x -> new GameMinDTO(x)).toList();
 	}
 	
